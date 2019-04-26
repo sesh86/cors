d54a9dcdd0d92bda40037d09480518486ef247a6
sesh86 <s.seshachalam@gmail.com>
2019-04-26 08:54:52 +0530

    cors-demo

---
+++ cors.js
@@ -0,0 +1,24 @@
+// const _app=require('./config.js')
+const express=require('express')
+const jwt = require('jsonwebtoken');
+
+app=express();
+
+app.get('/',(req,res)=>{
+    var user={id:1,user:'sesh'}
+    jwt.sign(user,'secret',
+    // {expiresIn:10},
+    (err,token)=>{
+      res.status(200).json(token);
+    })
+  });
+  app.get('/api/:token',(req,res)=>{
+    var decoded = jwt.decode(req.params.token);
+    if(decoded.exp<Date.now()){
+      res.send('Token Expired');
+    }
+    res.send(decoded);
+  })  
+  app.listen(3000);
+
+  console.log('Application is running on http://localhost:'+ 3000);  
